de53728c9d98e42edef7bafe76ede572b46f3394
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-10-11 14:40:18 +0900

    Update About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,8 @@ function About(props) {
         “Freedom is the freedom to say that two plus two make four. If that is
         granted, all else follows.”
       </span>
-      <span>− George Orwell, 1984</span>
+      
+      <span>George Orwell, 1984</span>
     </div>
   );
 }
